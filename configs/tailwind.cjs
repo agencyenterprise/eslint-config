bc13b63e858c0d646e43a655840dec21b6c94513
Daniel Cruz <danielcunha54@gmail.com>
2024-06-21 14:58:22 -0300

    refactor: set callees in the plugin settings

--- configs/tailwind.cjs
+++ configs/tailwind.cjs
@@ -4,15 +4,10 @@ const callees = ['classnames', 'clsx', 'ctl', 'cva', 'tv', 'cn'];
 
 /** @type import("eslint").Linter.Config */
 module.exports = {
-  plugins: ['tailwindcss'],
-  rules: {
-    'tailwindcss/classnames-order': ['warn', { callees }],
-    'tailwindcss/enforces-negative-arbitrary-values': ['warn', { callees }],
-    'tailwindcss/enforces-shorthand': ['warn', { callees }],
-    'tailwindcss/migration-from-tailwind-2': ['warn', { callees }],
-    'tailwindcss/no-arbitrary-value': ['off', { callees }],
-    'tailwindcss/no-custom-classname': ['warn', { callees }],
-    'tailwindcss/no-contradicting-classname': ['error', { callees }],
-    'tailwindcss/no-unnecessary-arbitrary-value': ['warn', { callees }],
+  extends: 'plugin:tailwindcss/recommended',
+  settings: {
+    tailwindcss: {
+      callees,
+    },
   },
 };
