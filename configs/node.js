2d93a9ab0af418c03dc035ea672187e77b51c168
Daniel Cruz <danielcunha54@gmail.com>
2024-06-21 12:59:59 -0300

    feat: create base config

--- configs/node.js
+++ configs/node.js
@@ -4,42 +4,5 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  extends: [
-    "standard",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
-  plugins: ["@typescript-eslint", "simple-import-sort"],
-  rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        printWidth: 100,
-        tabWidth: 2,
-        singleQuote: true,
-        trailingComma: "all",
-        arrowParens: "always",
-        semi: true,
-      },
-    ],
-    "simple-import-sort/imports": "error",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        vars: "all",
-        args: "after-used",
-        argsIgnorePattern: "^_",
-      },
-    ],
-  },
-  settings: {
-    "import/parsers": {
-      [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
-    },
-  },
+  extends: ["./base.js"],
 };
--- configs/react.js
+++ configs/react.js
@@ -5,36 +5,19 @@ module.exports = {
     es2021: true,
     jest: true,
   },
-  extends: [
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "standard",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
   },
-  plugins: ["react", "jsx-a11y", "@typescript-eslint", "simple-import-sort"],
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "./base.js",
+  ],
+  plugins: ["react", "jsx-a11y"],
   rules: {
     "react/self-closing-comp": "error",
-    "prettier/prettier": [
-      "error",
-      {
-        printWidth: 100,
-        tabWidth: 2,
-        singleQuote: true,
-        trailingComma: "all",
-        arrowParens: "always",
-        semi: true,
-        endOfLine: "auto",
-      },
-    ],
     "react/react-in-jsx-scope": "off",
     "react/prop-types": "off",
     "jsx-a11y/alt-text": [
@@ -50,22 +33,10 @@ module.exports = {
     "jsx-a11y/role-has-required-aria-props": "warn",
     "jsx-a11y/role-supports-aria-props": "warn",
     "react/no-unknown-property": "error",
-    "simple-import-sort/imports": "error",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        vars: "all",
-        args: "after-used",
-        argsIgnorePattern: "^_",
-      },
-    ],
   },
   settings: {
     react: {
       version: "detect",
-    },
-    "import/parsers": {
-      [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
     },
   },
   ignorePatterns: ["node_modules"],
