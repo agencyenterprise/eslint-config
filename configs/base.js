8ee8a3a9cd9d803c837d313e87ceedd8cc5dcc5c
Daniel Cruz <danielcunha54@gmail.com>
2024-06-21 13:59:21 -0300

    chore: use eslint node config

--- configs/base.js
+++ configs/base.js
@@ -1,41 +1,37 @@
 /** @type import("eslint").Linter.Config */
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  extends: [
-    "standard",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
-  plugins: ["@typescript-eslint", "simple-import-sort"],
+  extends: ['standard', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  plugins: ['@typescript-eslint', 'simple-import-sort'],
   rules: {
-    "prettier/prettier": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {
         printWidth: 100,
         tabWidth: 2,
         singleQuote: true,
-        trailingComma: "all",
-        arrowParens: "always",
+        trailingComma: 'all',
+        arrowParens: 'always',
         semi: true,
       },
     ],
-    "simple-import-sort/imports": "error",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
+    'simple-import-sort/imports': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
       {
-        vars: "all",
-        args: "after-used",
-        argsIgnorePattern: "^_",
+        vars: 'all',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
       },
     ],
   },
   settings: {
-    "import/parsers": {
-      [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
     },
   },
 };
